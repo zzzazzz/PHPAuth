69c754ad8414890f977df8ccfb7ac76be56e9ce8
Fa_b <zzzazzz@gmail.com>
2019-10-30 18:46:41 +0100

    Create br_FR.php
    New language, breton from Little Britany (West of France)

---
+++ languages/br_FR.php
@@ -0,0 +1,74 @@
+<?php
+$lang = array();
+
+$lang['user_blocked'] = "N’eo ket gweredekaet ho kont.";
+$lang['user_verify_failed'] = "Kod captcha direizh.";
+
+$lang['email_password_invalid'] = "Chomlec’h postel / ger-tremen divarrek.";
+$lang['email_password_incorrect'] = "Chomlec’h postel / ger-tremen direizh.";
+$lang['remember_me_invalid'] = "Ar vaezienn « chom en eñvor » zo direizh.";
+
+$lang['password_short'] = "Re verr eo ar ger-tremen-mañ.";
+$lang['password_weak'] = "Re ziasur eo ar ger-tremen-mañ.";
+$lang['password_nomatch'] = "N’eo ket peurheñvel ar gerioù-tremen.";
+$lang['password_changed'] = "Nevesaet eo bet ho ker-tremen.";
+$lang['password_incorrect'] = "Direizh eo ar ger-tremen-mañ.";
+$lang['password_notvalid'] = "Divarrek eo ar ger-tremen-mañ.";
+
+$lang['newpassword_short'] = "Re verr eo ar ger-tremen nevez-mañ.";
+$lang['newpassword_long'] = "Re hir eo ar ger-tremen-mañ.";
+$lang['newpassword_invalid'] = "Ret eo lakaat d’an nebeutañ ur bennlizherenn, ul lizherenn vunut hag ur sifr evit krouiñ ar ger-tremen nevez.";
+$lang['newpassword_nomatch'] = "Ar gerioù-tremen nevez n’int ket peurheñvel.";
+$lang['newpassword_match'] = "Ar ger-tremen nevez zo ar memes hini hag an hini kozh.";
+
+$lang['email_short'] = "Re verr eo ar chomlec’h postel";
+$lang['email_long'] = "Re hir eo ar chomlec’h postel.";
+$lang['email_invalid'] = "Divarrek eo ar chomlec’h postel.";
+$lang['email_incorrect'] = "Direizh eo ar chomlec’h postel.";
+$lang['email_banned'] = "Difennet eo implijout ar chomlec’h postel-mañ.";
+$lang['email_changed'] = "Enrollet mat eo bet ho chomlec’h postel.";
+
+$lang['newemail_match'] = "Ar chomlec’h postel nevez zo ar memes hini hag an hini kozh.";
+
+$lang['account_inactive'] = "N’eo ket gweredekaet c’hoazh ho kont.";
+$lang['account_activated'] = "Gweredekaet eo ho kont bremañ.";
+
+$lang['logged_in'] = "Kennasket oc’h bet bremañ.";
+$lang['logged_out'] = "Digevreet oc’h bremañ.";
+
+$lang['system_error'] = "C’hoarvezet ez eus ur fazi sistem. Klaskit en-dro.";
+
+$lang['register_success'] = "Krouet eo bet ar gont. Ur postel kadarnaat zo bet kaset deoc’h.";
+$lang['register_success_emailmessage_suppressed'] = "Krouet eo bet ar gont.";
+$lang['email_taken'] = "Ar chomlec’h postel-mañ a zo bet graet gantañ dija.";
+
+$lang['resetkey_invalid'] = "Divarrek eo an alc’hwez adnevesaat.";
+$lang['resetkey_incorrect'] = "Direizh eo an alc’hwez adnevesaat.";
+$lang['resetkey_expired'] = "Diamzeriet eo an alc’hwez adnevesaat.";
+$lang['password_reset'] = "Adnevesaet eo bet ho ker-tremen.";
+
+$lang['activationkey_invalid'] = "Divarrek eo an alc’hwez lañsañ.";
+$lang['activationkey_incorrect'] = "Direizh eo an alc’hwez lañsañ.";
+$lang['activationkey_expired'] = "Diamzeriet eo an alc’hwez lañsañ.";
+
+$lang['reset_requested'] = "Kaset ez eus bet ur goulenn deoc’h evit adnevesaat ho ker-tremen.";
+$lang['reset_requested_emailmessage_suppressed'] = "Krouet ez eus bet ur goulenn evit adnevesaat ho ker-tremen.";
+$lang['reset_exists'] = "Kaset ez eus bet ur goulenn evit adnevesaat ho ker-tremen dija.";
+
+$lang['already_activated'] = "Gweredekaet ha lañset eo ar gont.";
+$lang['activation_sent'] = "Kaset eo bet ar postel gweredekaat.";
+$lang['activation_exists'] = "Kaset eo bet dija ar postel gweredekaat.";
+
+$lang['email_activation_subject'] = '%s – Lañsañ ar gont';
+$lang['email_activation_body'] = 'Mat ar jeu ?<br><br>Evit mont war ho kont eo ret deoc’h krouiñ  anezhi da vat en ur glikañ war al liamm-mañ : :<strong><a href="%1$s/%2$s">%1$s/%2$s</a></strong><br/><br/> Ret vo deoc’h implijout an alc’hwez lañsañ-mañ :  <strong>%3$s</strong><br/><br/>\n Ma ne fell ket deoc’h enrollañ ho kont na rit ket van eus ar gemennadenn-mañ.';
+$lang['email_activation_altbody'] = "Mat ar jeu ?" . "\n\n" . 'Evit mont war ho kont eo ret deoc’h krouiñ anezhi da vat  en ur glikañ war al liamm-mañ : :%1$s/%2$s  Ret vo deoc’h implijout an alc’hwez lañsañ-mañ :   %3$s Ma ne fell ket deoc’h en em enrollañ war %1$s na rit ket van eus ar gemennadenn-mañ.';
+
+$lang['email_reset_subject'] = '%s - Adnevezañ ar ger-tremen';
+$lang['email_reset_body'] = 'Mat ar jeu ? <br/><br/> Evit adnevesaat ho ker-tremen eo ret deoc’h klikañ war al liamm-mañ :<br/><br/><strong><a href="%1$s/%2$s">%1$s/%2$s</a></strong><br/><br/> Ret vo deoc’h implijout an alc’hwez-mañ evit adnevesaat ho ker-tremen : <strong>%3$s</strong><br/><br/> Ma n’ho peus ket goulennet e vefe adnevesaet ho ker-tremen na rit ket van eus ar gemennadenn-mañ.';
+$lang['email_reset_altbody'] = 'Mat ar jeu ?' . "\n\n" . 'Evit adnevesaat ho ker-tremen eo ret deoc’h klikañ war al liamm-mañ :' . "\n" . '%1$s/%2$s' . "\n\n" . 'Ret vo deoc’h implijout an alc’hwez-mañ evit adnevesaat ho ker-tremen evit ar chomlec’h-mañ :  %1$s' . "\n\n" . 'Ma n’ho peus ket goulennet e vefe adnevesaet ho ker-tremen na rit ket van eus ar gemennadenn-mañ.';
+
+$lang['account_deleted'] = "Lamet eo ar gont.";
+$lang['function_disabled'] = "Lamet eo bet an doare-mañ d’ober.";
+$lang['account_not_found'] = "N’eus ket kont ebet.";
+
+return $lang;
